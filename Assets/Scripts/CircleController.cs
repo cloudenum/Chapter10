b8b047375993e8fcc960d86fc29ed5aba455a4b2
cloudenum <23111954+cloudenum@users.noreply.github.com>
2021-06-17 12:16:42 +0700

    Problem-4: Improved

--- Assets/Scripts/CircleController.cs
+++ Assets/Scripts/CircleController.cs
@@ -7,10 +7,7 @@ public class CircleController : MonoBehaviour
     private static ILogger logger = Debug.unityLogger;
     private static string TAG = "CircleSpeed";
     private Rigidbody2D rigidBody2D;
-    private Vector2 goUp = new Vector2(0f, 1f);
-    private Vector2 goDown = new Vector2(0f, -1f);
-    private Vector2 goLeft = new Vector2(-1f, 0f);
-    private Vector2 goRight = new Vector2(1f, 0f);
+    private Vector2 direction = new Vector2();
     private Vector2 stop = new Vector2(0, 0);
 
     // Start is called before the first frame update
@@ -20,33 +17,17 @@ public class CircleController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            move(goUp);
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            move(goDown);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            move(goLeft);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            move(goRight);
-        }
-        else
-        {
-            move(stop);
-            // rigidBody2D.velocity = new Vector2(0, 0);
-        }
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Move(x, y);
     }
 
-    private void move(Vector2 direction)
+    private void Move(float x, float y)
     {
+        direction.Set(x, y);
         // rigidBody2D.AddForce(direction * speed);
         rigidBody2D.velocity = direction * speed;
 
